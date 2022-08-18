e0fb82ceacd2591459cc8eb14401833678bea463
james.hornsby <james.hornsby@AFSWWERKUFGWGE.us.accenturefederal.com>
2022-08-18 16:10:58 -0500

    Step 4

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pluralsight.blog.data.PostRepository;
@@ -25,6 +26,13 @@ public class BlogController {
 		List<Post> pR = postRepository.getAllPosts();
 		mp.put("posts", pR);
 		return "home";
+	}
+	
+	@RequestMapping("/post/{id}")
+	public String postDetails(@PathVariable Long id, ModelMap modelMap) {
+		Post p = postRepository.findById(id);
+		modelMap.put("post", p);
+		return "post-details";
 	}
 	
 }
